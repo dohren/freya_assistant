a9843e1d864f9414d51d232302fd371cc0eff909
Andreas Dohren <dohren@web.de>
2024-04-26 17:00:58 +0200

    Musik sotppen

--- skills/Media/Spotify/__init__.py
+++ skills/Media/Spotify/__init__.py
@@ -12,25 +12,27 @@ redirect_uri = "http://localhost:8888/callback"
 
 
 def execute_skill(action, values):
-    song_name = values["song"]
     # Authentifizierung bei Spotify
     auth_manager = SpotifyOAuth(client_id=client_id,
                                 client_secret=client_secret,
                                 redirect_uri=redirect_uri,
                                 scope="user-read-playback-state user-modify-playback-state")
     sp = spotipy.Spotify(auth_manager=auth_manager)
-
-    # Suche das Lied
-    results = sp.search(q=song_name, limit=1)
-    if results['tracks']['items']:
-        # Hole die erste Übereinstimmung
-        track_uri = results['tracks']['items'][0]['uri']
-
-        # Spiele das Lied ab
-        sp.start_playback(uris=[track_uri])
-        print(f"Playing {song_name}")
+    if action == "stop_music":
+        sp.pause_playback()
     else:
-        print("Song not found.")
+        song_name = values["song"]
+        # Suche das Lied
+        results = sp.search(q=song_name, limit=1)
+        if results['tracks']['items']:
+            # Hole die erste Übereinstimmung
+            track_uri = results['tracks']['items'][0]['uri']
+
+            # Spiele das Lied ab
+            sp.start_playback(uris=[track_uri])
+            print(f"Playing {song_name}")
+        else:
+            print("Song not found.")
 
 
 if __name__ == "__main__":
